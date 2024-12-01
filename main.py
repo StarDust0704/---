8340de7c439c9c8f54223e20302f482bc2a9f6fa
StarDust0704 <fan19630704@gmail.com>
2024-12-01 11:51:52 +0300

    Переменные

---
+++ main.py
@@ -0,0 +1,16 @@
+ # Домашнее задание _1_3 Тема:Переменные
+
+# Определение переменных
+completed_homeworks = 12       # Количество выполненных ДЗ
+spent_hours = 1.5              # Количество затраченных часов
+course_title = 'Python'        # Название курса
+time_per_task = spent_hours / completed_homeworks  # Время на одно задание
+
+# Вывод строки на экран
+print(f'Курс: {course_title}, всего задач: {completed_homeworks}, затрачено часов: {spent_hours}, среднее время выполнения {time_per_task:.3f} часа.')
+
+
+
+
+
+
